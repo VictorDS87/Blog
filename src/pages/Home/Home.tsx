a0a07c9ec6dc690bdba815a8ed1583d2e4a4ab67
Victor Donizete <victordonizete65@gmail.com>
2023-03-18 13:13:56 -0300

    finished style for Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,43 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header/Header";
 import { Profile } from "../../components/Profile/Profile";
-import { HomeContainer } from "./styles";
+import { SearchForm } from "../../components/SearchForm/SearchForm";
+import { BlogContext } from "../../context/BlogContext";
+import {dateFormatter} from "../../utils/Formatter";
+
+import { Body, CardBlog, DateBlog, HomeContainer, SummaryBlogContainer, Title } from "./styles";
+
 
 export function Home() {
+    const { blogs } =  useContext(BlogContext)
+
+    function teste() {
+        alert(blogs[0].title)
+    }
+    
     return (
         <HomeContainer>
             <Header />
             <Profile />
+            <SearchForm/>
+
+            <SummaryBlogContainer>
+                
+                {blogs.map(blog => {
+    
+                        return (                                      
+                            <CardBlog onClick={teste} key={blog.id}>                          
+                                <Title>                                                                                             
+                                        {blog.title}           
+                                    <DateBlog>                                 
+                                        {dateFormatter.format(new Date(blog.updated_at))}
+                                    </DateBlog>                                   
+                                </Title>
+                                <Body>{blog.body}</Body>                               
+                            </CardBlog>                                        
+                        )                  
+                })}
+            </SummaryBlogContainer>
         </HomeContainer>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,9 +1,81 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
-
     width: 100%;
-    max-width: 1800px;
-    margin: 0 auto 0;
+    max-width: 1980px;
+
+`
+
+export const SummaryBlogContainer = styled.main`
+    width: 100%;
+    max-width: 1173px;
+    margin: 0 auto;  
+    margin-top: 2rem;
+
+    columns: 2 auto; 
+`
+
+export const CardBlog = styled.div`
+    /* display: flex; */
     
+    text-overflow: ellipsis; 
+    
+    width: 100%;
+    
+
+    /* Base/Post */
+
+    background: #112131;
+    border-radius: 10px;
+    white-space: normal; 
+    
+    overflow: hidden;
+    text-overflow: clip; 
+
+    cursor: pointer;
+
+    transition: 0.3s;
+    &:hover {
+        filter: brightness(50%)
+    }
+    
+`
+
+export const Title = styled.div`
+    font-family: 'Nunito', sans-serif;
+    font-weight: 700;
+
+    font-size: 1.15rem;
+    margin: 1rem;
+
+    display: flex;
+    /* align-content: center; */
+    align-items: center;
+    justify-content: space-between;
+
+    color: ${props => props.theme.title};
+`
+
+export const DateBlog = styled.p`
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+ 
+    margin-top: 1rem;
+    font-size: 0.75rem;
+
+    color: ${props => props.theme.subtitle};
+`
+
+export const Body = styled.p`
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+
+    overflow: hidden; 
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 4; 
+    -webkit-box-orient: vertical; 
+    margin: 1rem;
+
+    color: ${props => props.theme.text};
 `
