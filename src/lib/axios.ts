71aa7e061386fccd31a45d0d2cf3cae258aa9f1f
Victor Donizete <victordonizete65@gmail.com>
2023-03-18 13:13:10 -0300

    api for show profile and show blogs

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,3 +3,8 @@ import axios from 'axios'
 export const api = axios.create({
     baseURL: 'https://api.github.com/users/',
 })
+
+export const apiBlog = axios.create({
+    baseURL: 'https://api.github.com/search/issues?q=Boas%20pr%C3%A1ticas%20repo:rocketseat-education/',
+    
+})
