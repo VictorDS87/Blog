060fb4c22b98794463663c6c4416e7d20c3d6eaf
Victor Donizete <victordonizete65@gmail.com>
2023-03-18 13:15:29 -0300

    add style for web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "styled-components";
-import { BlogProvider } from "./context/BlogTransactions";
+import { BlogProvider } from "./context/BlogContext";
 import { Home } from "./pages/Home/Home";
+import { GlobalStyle } from "./styles/global";
 import { DefaultTheme } from "./styles/theme/default";
 
 export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
+      <GlobalStyle/>
       <BlogProvider>
         <Home />
-
       </BlogProvider>
     </ThemeProvider>
   )
--- src/styles/theme/default.ts
+++ src/styles/theme/default.ts
@@ -1,20 +1,16 @@
 export const DefaultTheme = {
     white: '#fff',
   
-    'gray-100': '#E1E1E6',
-    'gray-300': '#C4C4CC',
-    'gray-400': '#8D8D99',
-    'gray-500': '#7C7C8A',
-    'gray-600': '#323238',
-    'gray-700': '#29292E',
-    'gray-800': '#202024',
-    'gray-900': '#121214',
+    input: '#040F1A',
+    background: '#071422',
+    profile: '#0B1B2B',
+    post: '#112131',
+    border: '#1C2F41',
+    label: '#3A536B',
+    span: '#7B96B2',
+    text: '#AFC2D4',
   
-    'green-300': '#00B37E',
-    'green-500': '#00875F',
-    'green-700': '#015F43',
-  
-    'red-300': '#F75A68',
-    'red-500': '#AB222E',
-    'red-700': '#7A1921',
+    subtitle: '#C4D4E3',
+    title: '#E7EDF4',
+    blue: '#3294F8',
   } as const
