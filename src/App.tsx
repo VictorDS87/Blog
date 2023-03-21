14e4f90c2af26a0255a27f6cfd641acadaf83541
Victor Donizete <victordonizete65@gmail.com>
2023-03-21 13:14:18 -0300

    BrowserRouter add in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { BlogProvider } from "./context/BlogContext";
 import { Home } from "./pages/Home/Home";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { DefaultTheme } from "./styles/theme/default";
 
@@ -8,9 +10,12 @@ export function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
       <GlobalStyle/>
-      <BlogProvider>
-        <Home />
-      </BlogProvider>
+      <BrowserRouter>
+        <BlogProvider>
+          <Router/>
+        </BlogProvider>
+      </BrowserRouter>
+      
     </ThemeProvider>
   )
 }
